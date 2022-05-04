31d2011ceb9129344457e627148ac7196c4e86a9
Yasser <yasserbal@icloud.com>
2022-05-04 16:46:45 +0200

    It works....Come back to this

--- 02-colors/01-change-bcg-one/script.js
+++ 02-colors/01-change-bcg-one/script.js
@@ -9,8 +9,61 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
 
-})();
+// your code here
+
+
+
+let btnRed = document.getElementById('red');
+let btnGreen = document.getElementById('green');
+let btnYellow = document.getElementById('yellow');
+let btnBlue = document.getElementById('blue');
+btnRed.addEventListener('click',changeBgToRed);
+btnGreen.addEventListener('click',changeBgToGreen);
+btnYellow.addEventListener('click',changeBgToYellow);
+btnBlue.addEventListener('click',changeBgToBlue);
+function changeBgToRed(){
+    document.body.style.backgroundColor = "red"
+}
+function changeBgToGreen(){
+    document.body.style.backgroundColor = "green"
+}
+function changeBgToYellow(){
+    document.body.style.backgroundColor = "yellow"
+}
+function changeBgToBlue(){
+    document.body.style.backgroundColor = "blue"
+}
+// let buttons = []
+// buttons.concat(document.getElementsByTagName('button'))
+// for(i in buttons){
+//     // console.log(buttons[i].getAttribute('id'))
+//     // buttons[i].addEventListener('click',test())
+//     console.log(buttons[i])
+// }
+//     let buttons = [document.getElementsByTagName('button')]
+//     console.log(buttons)
+//     buttons.forEach(btn => {
+//         console.log(btn)
+//         console.log('NEXT')
+//         btn.innerHTML = 'no'
+
+// //     });
+// function test(){
+//     console.log('lalal')
+// }
+// function changeBackgroundColor($clr) {
+//     console.log($clr)
+//     switch ($clr) {
+//         case red:
+//             document.body.style.backgroundColor = "red";
+//         case green:
+//             document.body.style.backgroundColor = "green";
+//         case yellow:
+//             document.body.style.backgroundColor = "yellow";
+//         case blue:
+//             document.body.style.backgroundColor = "blue";
+
+//     }
+// }
