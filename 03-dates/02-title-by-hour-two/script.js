bfc1308c2bcbb9397657e6bf6cfdfa65622ef23a
Yasser <yasserbal@icloud.com>
2022-05-04 17:46:41 +0200

    Yay Copy Paste

--- 03-dates/02-title-by-hour-two/script.js
+++ 03-dates/02-title-by-hour-two/script.js
@@ -14,5 +14,18 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    //Sorry got to copy paste that stuff
+    let target = document.getElementById('target');
+    //Get System date ? since 1970 apparently ?
+    const date = new Date();
+    // Does return current hour ???
+    let currentHour = date.getHours();
+    // Returns current minutes based on timezone
+    let currentMinutes = date.getMinutes();
+    if(currentHour <= 18 && currentMinutes<=30 ){
+        target.innerHTML = "<i>Hello</i> it's: " + currentHour + ":" + currentMinutes;
+    }else{
+        target.innerHTML = "<i>Good Evening</i> it's: " + currentHour + ":" + currentMinutes;
+    }
 
 })();
