ce609413cc601b039ab8d635ed3d990c127817c1
Yasser <yasserbal@icloud.com>
2022-05-04 18:15:46 +0200

    Days and months are another level

--- 03-dates/03-age-by-select/script.js
+++ 03-dates/03-age-by-select/script.js
@@ -12,5 +12,38 @@
 (function() {
 
     // your code here
+    //Get User Input
+    let dobDayInput = document.getElementById('dob-day');
+    let dobMonthInput = document.getElementById('dob-month');
+    let dobYearInput = document.getElementById('dob-year');
+    const btnRun = document.getElementById('run');
+    const target = document.getElementById('target');
 
+    //Get Current Date
+    const date = new Date();
+    // Days and months must be in an array! Sneaky....
+    let currentDay = date.getDay() +1;
+    let currentMonth = date.getMonth() +1;
+    let currentYear = date.getFullYear();
+
+
+
+
+
+    btnRun.addEventListener('click',calculateAge)
+    
+    function calculateAge(){
+        //Cast User input to usable variables
+        let dobDay = dobDayInput.value;
+        let dobMonth = dobMonthInput.value;
+        let dobYear = dobYearInput.value;
+        //Calculate current Age
+        let visitorAgeYears = currentYear - dobYear;
+        let visitorAgeMonths = currentMonth - dobMonth;
+        let visitorAgeDays = currentDay - dobDay;
+        //Cast it to target
+        let message = "You are: " + visitorAgeDays + " Days," + visitorAgeMonths + " Months," + visitorAgeYears + "years young! Still looking good :D";
+        target.innerHTML = message;
+        //At least I got the year right :D
+    }
 })();
