83c42bdfeb79dcef1c1e407dde68a55d0c155131
Yasser <yasserbal@icloud.com>
2022-05-04 18:30:38 +0200

    Did I need an Array ?

--- 03-dates/04-locale-date-Optional/script.js
+++ 03-dates/04-locale-date-Optional/script.js
@@ -14,5 +14,30 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    let target = document.getElementById('target');
+    let date = new Date();
+    let day = date.getDay()+1;
+    let month = date.getDay()+1;
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    dateToEnglish();
+
+    function dateToEnglish(){
+        let daysInEnglish = "Today's Date: ";
+        daysInEnglish += days[day];
+        daysInEnglish += " ";
+        daysInEnglish += months[month];
+        daysInEnglish += " ";
+        daysInEnglish += year;
+        daysInEnglish += ", "
+        daysInEnglish += hours;
+        daysInEnglish += "h";
+        daysInEnglish += minutes;
+        target.innerHTML = daysInEnglish;
+    }
+    
 
 })();
