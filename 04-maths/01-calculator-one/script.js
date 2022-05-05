c6d4fad2bf6d60a406a35b640146dd69d798fe45
YasserB94 <yasserbal@icloud.com>
2022-05-05 10:44:15 +0200

    Added Calculator logic

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -8,23 +8,55 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-
+//Get all elements needed
+const opOne = document.getElementById('op-one');
+const opTwo = document.getElementById('op-two');
+const target = document.getElementById('target');
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        // Get values and turn them into integers
+        const opOneValue = parseInt(opOne.value);
+        const opTwoValue = parseInt(opTwo.value);
+        //Calculate result
+        let result = (opOneValue+opOneValue);
+        //Cast result to target
+        target.innerHTML = opOneValue + "+"  + opTwoValue + " = " + result;
+
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        // Get values and turn them into integers
+        const opOneValue = parseInt(opOne.value);
+        const opTwoValue = parseInt(opTwo.value);
+        //Calculate result
+        let result = (opOneValue-opOneValue);
+        //Cast result to target
+        target.innerHTML = opOneValue + "-"  + opTwoValue + " = " + result;
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        // Get values and turn them into integers
+        const opOneValue = parseInt(opOne.value);
+        const opTwoValue = parseInt(opTwo.value);
+        //Calculate result
+        let result = (opOneValue*opOneValue);
+        //Cast result to target
+        target.innerHTML = opOneValue + "*"  + opTwoValue + " = " + result;
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        // Get values and turn them into integers
+        const opOneValue = parseInt(opOne.value);
+        const opTwoValue = parseInt(opTwo.value);
+        //Calculate result
+        let result = (opOneValue/opOneValue);
+        //Cast result to target
+        target.innerHTML = opOneValue + "/"  + opTwoValue + " = " + result;
     });
 })();
