f5a4d2534c161d169c78403e36bdaa6db6260f4b
YasserB94 <yasserbal@icloud.com>
2022-05-05 11:48:44 +0200

    The famous fizzbuzz modulus

--- 04-maths/03-fizzbuzz/script.js
+++ 04-maths/03-fizzbuzz/script.js
@@ -12,5 +12,32 @@
 (function() {
 
     // your code here
+    for(i = 0;i<=100;i++){
+        //check if number is a multiple of 3 and 5
+        if(i%3==0 && i%5==0){
+            logFizzBuzz()
+        }
+        //check if number is a multiple of 3
+        else if(i%3==0){
+            logFizz()
+        }
+        //check if number is a multiple of 5
+        else if(i%5==0){
+            logBuzz()
+        }
+        //Log the number
+        else{
+            console.log(i);
+        }
+    }
 
 })();
+function logFizz(){
+    console.log("fizz")
+}
+function logBuzz(){
+    console.log("buzz")
+}
+function logFizzBuzz(){
+    console.log("fizzbuzz")
+}
