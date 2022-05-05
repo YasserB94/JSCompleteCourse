006fcfd32dc2f3bde33b2d1a7b6e5051339eb27a
YasserB94 <yasserbal@icloud.com>
2022-05-05 11:59:35 +0200

    Console has the results

--- 04-maths/04-even-square-Optional/script.js
+++ 04-maths/04-even-square-Optional/script.js
@@ -8,13 +8,18 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-
+const target = document.getElementById('target');
 (function() {
-
+    
     document.getElementById("run").addEventListener("click", function() {
 
         // your code here
-
+        //For loop to go trough numbers 1 to 21
+        for(i = 1;i<=21;i++){
+            let result = i*i;
+            console.log(result)
+            target.innerHTML = "The square of: " + i + " = " + result;
+        }
     });
 
 })();
