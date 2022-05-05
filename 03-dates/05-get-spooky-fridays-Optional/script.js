148058afdc5c86ccb57816808d093ea6b05bc5c1
Yasser <yasserbal@icloud.com>
2022-05-05 07:35:37 +0200

    WiP

--- 03-dates/05-get-spooky-fridays-Optional/script.js
+++ 03-dates/05-get-spooky-fridays-Optional/script.js
@@ -9,8 +9,35 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     // your code here
+    //Fetch Year Input
+
+    //Have a target field to display months
+    let target = document.getElementById('target');
+    const btnRun = document.getElementById('run');
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let userInputYear = document.getElementById('year');
+    let monthsThatHaveFridayThe13th = [];
+    btnRun.addEventListener('click',updateTarget)
+    //Logic to see if it's friday the 13th
+    let date = new Date();
+
+
+    function updateTarget(){
+        //Set the year from users input as the year date will look trough
+        date.setFullYear(userInputYear);
+        //Loop trough everymonth
+        
+        for(i = 0;i < months.length;i++){
+            
+            console.log(date.getDay())    
+        }
+    }
+
+
+
+
 
 })();
