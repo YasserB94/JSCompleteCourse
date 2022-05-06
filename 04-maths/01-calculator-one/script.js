723626b9e1dc34a33cd4be2f9d031a11dfdac257
Yasser <yasserbal@icloud.com>
2022-05-06 13:59:28 +0200

    Completed Calculator One

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -8,6 +8,57 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+
+//Listening to the addition button for a click,Since we don't want to use this logic anywhere else we can implement the function in the event listener
+document.getElementById('addition').addEventListener('click', function () {
+    //Let's get the values of our 2 fields
+    //User input is a string, so we have to parse them to an int
+    //We get the text-field from the dom and ask for it's value.
+    //We want to get the value when it's clicked so we declare these within the funciton
+    const opOne = parseInt(document.getElementById('op-one').value);
+    const opTwo = parseInt(document.getElementById('op-two').value);
+    //I added a target id to my html to display the result on the webpage
+    //Let's see if the result is a number if it is let's display the result
+    if (isNaN(opOne + opTwo)) {
+        //isNaN returned true, its NotaNumber!
+        document.getElementById('target').innerHTML = "Please enter numbers!"
+    } else {
+        //isNan returned false, we can show our results
+        document.getElementById('target').innerHTML = opOne + " + " + opTwo + " = " + (opOne + opTwo)
+    }
+});
+//Let's repeat this function for the other buttons changing the operator.
+document.getElementById('substraction').addEventListener('click', function () {
+    const opOne = parseInt(document.getElementById('op-one').value);
+    const opTwo = parseInt(document.getElementById('op-two').value);
+    if (isNaN(opOne - opTwo)) {
+        document.getElementById('target').innerHTML = "Please enter numbers!"
+    } else {
+        document.getElementById('target').innerHTML = opOne + " - " + opTwo + " = " + (opOne - opTwo)
+    }
+});
+document.getElementById('multiplication').addEventListener('click', function () {
+    const opOne = parseInt(document.getElementById('op-one').value);
+    const opTwo = parseInt(document.getElementById('op-two').value);
+    if (isNaN(opOne * opTwo)) {
+        document.getElementById('target').innerHTML = "Please enter numbers!"
+    } else {
+//Using a + to form our string and performing math in the string can make it a bit harder to read
+//We can use template/string literals to make the code easier to reead!
+        document.getElementById('target').innerHTML = `${opOne} * ${opTwo} = ${opOne * opTwo}`
+    }
+});
+document.getElementById('division').addEventListener('click', function () {
+    const opOne = parseInt(document.getElementById('op-one').value);
+    const opTwo = parseInt(document.getElementById('op-two').value);
+    if (isNaN(opOne / opTwo)) {
+        document.getElementById('target').innerHTML = "Please enter numbers!"
+    } else {
+        document.getElementById('target').innerHTML = `${opOne} / ${opTwo} = ${opOne / opTwo}`
+    }
+});
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*--OBSOLETE
 //Get all elements needed
 const opOne = document.getElementById('op-one');
 const opTwo = document.getElementById('op-two');
@@ -60,3 +111,4 @@ const target = document.getElementById('target');
         target.innerHTML = opOneValue + "/"  + opTwoValue + " = " + result;
     });
 })();
+--*/
