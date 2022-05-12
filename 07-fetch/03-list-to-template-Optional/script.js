c3fdf715ac3866f076bd929506b2e36d465f80ff
YasserB94 <yasserbal@icloud.com>
2022-05-12 11:10:00 +0200

    Assignment 07-03 complete

--- 07-fetch/03-list-to-template-Optional/script.js
+++ 07-fetch/03-list-to-template-Optional/script.js
@@ -11,4 +11,72 @@
 
 (() => {
     // your code here
+        document.getElementById('run').addEventListener('click', () => {
+            displayHeros();
+        })
+
 })();
+async function displayHeros() {
+    const data = await getHeroesFromAPI();
+    const heroes = data.heroes
+    for (i = 0; i < heroes.length; i++) {
+        const hero = heroes[i];
+        const name = hero.name;
+        const alterego = hero.alterego;
+        const heroPowers = hero.abilities.join(', ');
+        document.getElementById('target').appendChild(createHeroHtml(name, alterego, heroPowers));
+    }
+}
+async function getHeroesFromAPI() {
+    const url = '../../_shared/api.json';
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('There was an error getting the Heroes data');
+        console.log(`API url:${url}`);
+        console.log(`API fetch response: ${response}`);
+        console.log(`response.json:${data}`)
+        console.log(`Error: ${error}`)
+    }
+}
+
+function getHeroHTMLTemplate() {
+    try {
+        const template = document.getElementById('tpl-hero');
+        const clone = template.content.cloneNode(true);
+        return clone;
+    } catch (error) {
+        console.log('There was an error getting the template to display the heroes');
+        console.log(`Received template: ${template}`)
+        console.log(`cloneNode: ${clone}`)
+        console.log(`Error: ${error}`)
+    }
+}
+function createHeroHtml(name, alterego, powers) {
+    try {
+        const template = getHeroHTMLTemplate();
+        template.querySelector(`.name`).innerHTML = name;
+        template.querySelector(`.alter-ego`).innerHTML = alterego;
+        template.querySelector(`.powers`).innerHTML = powers;
+        return template;
+    } catch (error) {
+        console.log('There was an error filling in the template');
+        console.log(`Hero characteristics received:${name},${alterego},${powers}`)
+        console.log(`Template used:${template}`)
+        console.log(`Error:${error}`)
+    }
+}
+
+
+
+
+//dustinbred .com
+//INPUT SANITATION
+
+
+
+
+const myRandomText = document.createElement('p');
+myRandomText.innerHTML = 'Some random text';
